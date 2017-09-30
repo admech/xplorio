47f201d8ffa29e868cc47edf5c563136a396a548
admech <kiriger@gmail.com>
2017-09-30 18:03:18 +0300

    [POLISH] Removed unnecessary cast.

--- src/app/solution.service.ts
+++ src/app/solution.service.ts
@@ -21,7 +21,7 @@ const SOLUTIONS: XSolution[] = [
         series: [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8]]
       },
     ]
-  } as XSolution,
+  },
   {
     name: 'long diag',
     variables: [
@@ -38,7 +38,7 @@ const SOLUTIONS: XSolution[] = [
         series: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
       },
     ]
-  } as XSolution,
+  },
 ];
 
 @Injectable()
