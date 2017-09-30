23f8bbcd351d40966f7d245d3fe020038a7ccc6a
admech <kiriger@gmail.com>
2017-09-30 15:22:37 +0300

    [CLEANUP] Factored charts into a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as d3 from 'd3';
-
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/of';
-
-import { Updatable } from './updatable';
-
-import { ChartData, ChartDataEntry } from './chart-data';
-import { ChartDataService } from './chart-data.service';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,60 +7,9 @@ import { ChartDataService } from './chart-data.service';
 })
 export class AppComponent implements OnInit {
 
-  title = 'app';
-
-  private lineChartData: Observable<ChartDataEntry[]>;
-  private lineChartDataSubject = new Subject<void>();
-
-
-  constructor(
-    private chartDataService: ChartDataService
-  ) {}
+  constructor() { }
 
   ngOnInit() {
-    this.lineChartData = this.lineChartDataSubject
-      .switchMap(it => this.chartDataService.getChartData())
-      .catch(error => {
-        console.error('error on get chart data: ' + error);
-        return Observable.of([]);
-      });
-    this.newChart();
-  }
-
-  newChart() {
-    console.log('creating a new chart');
-    this.chartDataService.create()
-      .then(entry => {
-        console.log('created.');
-        this.lineChartDataSubject.next();
-        this.chartDataService.bringToFront(entry.index());
-      });
-  }
-
-  deleteChart(index: number) {
-    console.log('deleting chart #' + index);
-    this.chartDataService.delete(index)
-      .then(deleted => {
-        if (deleted) {
-          console.log('deleted chart #' + index);
-          this.lineChartDataSubject.next();
-        } else {
-          console.error('could not delete chart');
-        }
-      });
-  }
-
-  bringToFront(index: number): void {
-    console.log("Bringing to front chart #" + index);
-    this.chartDataService.bringToFront(index)
-      .then(broughtToFront => {
-        if (broughtToFront) {
-          console.log('broughtToFront chart #' + index);
-          this.lineChartDataSubject.next();
-        } else {
-          console.error('could not broughtToFront chart');
-        }
-      });
   }
 
 }
---
+++ src/app/charting.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as d3 from 'd3';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+
+import { Updatable } from './updatable';
+
+import { ChartData, ChartDataEntry } from './chart-data';
+import { ChartDataService } from './chart-data.service';
+
+
+@Component({
+  selector: 'charting-area',
+  templateUrl: './charting.component.html',
+  styleUrls: ['./charting.component.css']
+})
+export class ChartingComponent implements OnInit {
+
+  private lineChartData: Observable<ChartDataEntry[]>;
+  private lineChartDataSubject = new Subject<void>();
+
+  constructor(
+    private chartDataService: ChartDataService
+  ) {}
+
+  ngOnInit() {
+    this.lineChartData = this.lineChartDataSubject
+      .switchMap(it => this.chartDataService.getChartData())
+      .catch(error => {
+        console.error('error on get chart data: ' + error);
+        return Observable.of([]);
+      });
+    this.newChart();
+  }
+
+  newChart() {
+    console.log('creating a new chart');
+    this.chartDataService.create()
+      .then(entry => {
+        console.log('created.');
+        this.lineChartDataSubject.next();
+        this.chartDataService.bringToFront(entry.index());
+      });
+  }
+
+  deleteChart(index: number) {
+    console.log('deleting chart #' + index);
+    this.chartDataService.delete(index)
+      .then(deleted => {
+        if (deleted) {
+          console.log('deleted chart #' + index);
+          this.lineChartDataSubject.next();
+        } else {
+          console.error('could not delete chart');
+        }
+      });
+  }
+
+  bringToFront(index: number): void {
+    console.log("Bringing to front chart #" + index);
+    this.chartDataService.bringToFront(index)
+      .then(broughtToFront => {
+        if (broughtToFront) {
+          console.log('broughtToFront chart #' + index);
+          this.lineChartDataSubject.next();
+        } else {
+          console.error('could not broughtToFront chart');
+        }
+      });
+  }
+
+}
